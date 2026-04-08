11cba34e7e6a3c21b70cf14f0e23ad5599924cab
Oskar Kein <202301708@estudiante.udv.edu.gt>
2026-04-07 21:09:41 -0600

    otra prueba

--- src/main/java/com/example/demo/service/helloS.java
+++ src/main/java/com/example/demo/service/helloS.java
@@ -6,6 +6,6 @@ import org.springframework.stereotype.Service;
 public class helloS {
 
     public String getHelloMessage() {
-        return "de nuevooo voy";
+        return "prueba mas";
     }
 }
